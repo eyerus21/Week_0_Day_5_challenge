38fb532ea88d24ac5f146d701c5ef7d5a9255689
eyerus21 <eyugher21@gmail.com>
2021-06-27 20:19:24 +0300

    update

---
+++ Adding_Data.py
@@ -0,0 +1,216 @@
+import os
+import pandas as pd
+import mysql.connector as mysql
+from mysql.connector import Error
+
+def DBConnect(dbName=None):
+    """
+
+    Parameters
+    ----------
+    dbName :
+        Default value = None)
+
+    Returns
+    -------
+
+    """
+    conn = mysql.connect(host='localhost', user='root',
+                         database=dbName, buffered=True)
+    cur = conn.cursor()
+    return conn, cur
+
+def emojiDB(dbName: str) -> None:
+    conn, cur = DBConnect(dbName)
+    dbQuery = f"ALTER DATABASE {dbName} CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;"
+    cur.execute(dbQuery)
+    conn.commit()
+
+def createDB(dbName: str) -> None:
+    """
+
+    Parameters
+    ----------
+    dbName :
+        str:
+    dbName :
+        str:
+    dbName:str :
+
+
+    Returns
+    -------
+
+    """
+    conn, cur = DBConnect()
+    cur.execute(f"CREATE DATABASE IF NOT EXISTS {dbName};")
+    conn.commit()
+    cur.close()
+
+def createTables(dbName: str) -> None:
+    """
+
+    Parameters
+    ----------
+    dbName :
+        str:
+    dbName :
+        str:
+    dbName:str :
+
+
+    Returns
+    -------
+
+    """
+    conn, cur = DBConnect(dbName)
+    sqlFile = 'TWitter.sql'
+    fd = open(sqlFile, 'r')
+    readSqlFile = fd.read()
+    fd.close()
+
+    sqlCommands = readSqlFile.split(';')
+
+    for command in sqlCommands:
+        try:
+            res = cur.execute(command)
+        except Exception as ex:
+            print("Command skipped: ", command)
+            print(ex)
+    conn.commit()
+    cur.close()
+
+    return
+
+def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
+    """
+
+    Parameters
+    ----------
+    df :
+        pd.DataFrame:
+    df :
+        pd.DataFrame:
+    df:pd.DataFrame :
+
+
+    Returns
+    -------
+
+    """
+   
+    
+    df = df.drop(columns='possibly_sensitive', axis=1)
+    df = df.fillna(0)
+   
+
+    return df
+
+
+def insert_to_tweet_table(dbName: str, df: pd.DataFrame, table_name: str) -> None:
+    """
+
+    Parameters
+    ----------
+    dbName :
+        str:
+    df :
+        pd.DataFrame:
+    table_name :
+        str:
+    dbName :
+        str:
+    df :
+        pd.DataFrame:
+    table_name :
+        str:
+    dbName:str :
+
+    df:pd.DataFrame :
+
+    table_name:str :
+
+
+    Returns
+    -------
+
+    """
+    conn, cur = DBConnect(dbName)
+
+    
+
+    for _, row in df.iterrows():
+        sqlQuery = f"""INSERT INTO {table_name} (created_at, source, original_text, polarity, subjectivity, lang,
+                    favorite_count, retweet_count, original_author,followers_count, friends_count,
+                    hashtags, user_mentions, place)
+             VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s );"""
+        data = (row[0], row[1], row[2], row[3], (row[4]), (row[5]), row[6], row[7], row[8], row[9], row[10], row[11],
+                row[12], row[13])
+
+        try:
+            # Execute the SQL command
+            cur.execute(sqlQuery, data)
+            # Commit your changes in the database
+            conn.commit()
+            print("Data Inserted Successfully")
+        except Exception as e:
+            conn.rollback()
+            print("Error: ", e)
+    return
+
+def db_execute_fetch(*args, many=False, tablename='', rdf=True, **kwargs) -> pd.DataFrame:
+    """
+
+    Parameters
+    ----------
+    *args :
+
+    many :
+         (Default value = False)
+    tablename :
+         (Default value = '')
+    rdf :
+         (Default value = True)
+    **kwargs :
+
+
+    Returns
+    -------
+
+    """
+    connection, cursor1 = DBConnect(**kwargs)
+    if many:
+        cursor1.executemany(*args)
+    else:
+        cursor1.execute(*args)
+
+    # get column names
+    field_names = [i[0] for i in cursor1.description]
+
+    # get column values
+    res = cursor1.fetchall()
+
+    # get row count and show info
+    nrow = cursor1.rowcount
+    if tablename:
+        print(f"{nrow} recrods fetched from {tablename} table")
+
+    cursor1.close()
+    connection.close()
+
+    # return result
+    if rdf:
+        return pd.DataFrame(res, columns=field_names)
+    else:
+        return res
+
+
+if __name__ == "__main__":
+    createDB(dbName='tweets')
+    emojiDB(dbName='tweets')
+    createTables(dbName='tweets')
+
+    df = pd.read_csv('processed_tweet_data .csv')
+    
+    df=preprocess_df(df)
+    insert_to_tweet_table(dbName='tweets', df=df, table_name='Twitter')
---
+++ TWitter.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS `Twitter` 
+(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `created_at` TEXT NOT NULL,
+    `source` VARCHAR(200) NOT NULL,
+    `original_text` TEXT DEFAULT NULL,
+    `polarity` FLOAT DEFAULT NULL,
+    `subjectivity` FLOAT DEFAULT NULL,
+    `lang` TEXT DEFAULT NULL,
+    `favorite_count` INT DEFAULT NULL,
+    `retweet_count` INT DEFAULT NULL,
+    `original_author` TEXT DEFAULT NULL,
+    `followers_count` INT DEFAULT NULL,
+    `friends_count` INT DEFAULT NULL,
+    `hashtags` TEXT DEFAULT NULL,
+    `user_mentions` TEXT DEFAULT NULL,
+    `place` TEXT DEFAULT NULL,
+    
+    PRIMARY KEY (`id`)
+)
+ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
